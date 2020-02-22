5d111992d0e80054bd1104ddadecc2f05e356041
void <git.email.address@googlemail.com>
2020-02-23 00:55:24 +0100

    Finally made some progress with the menu.

--- client/source/client.ts
+++ client/source/client.ts
@@ -1,6 +1,8 @@
 import * as api from './api.js';
 
 window.addEventListener('DOMContentLoaded', async event => {
+    /*
     const response = await api.validateSession();
     console.log(response);
+    */
 });
